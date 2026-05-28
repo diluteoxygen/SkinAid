b4c1e1d7e2bece656950d8c2ceb753c4ba752a74
Vikrant Singh <vikrantsinghtomar011@gmail.com>
2026-05-28 08:27:31 +0530

    Initial Commit

--- backend/test_model.py
+++
@@ -1,8 +0,0 @@
-import os
-from clip_infer import analyze_image
-
-if __name__ == "__main__":
-    image_path = os.path.join(os.path.dirname(__file__), "test.jpg")
-    result = analyze_image(image_path)
-
-    print(result)
